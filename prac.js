06b23bf0895bfb6889439bc2a6be2d2ba79fda16
Delta Student <deltastudent@apnacollege.in>
2025-03-17 15:24:48 +0530

    question solved

--- prac.js
+++ prac.js
@@ -1,3 +1,16 @@
 let arr = [-9,-7,-6,-5,0,5,3,2];
 let op = [-5,5];
 
+let si = 0;
+let ei = arr.length-1;
+
+while(si < ei) {
+    let ans = arr[si] + arr[ei];
+    if(ans == 0) {
+        console.log([arr[si],arr[ei]])
+    } else if(ans > 0) {
+        ei++;
+    } else {
+        si--;
+    }
+}
